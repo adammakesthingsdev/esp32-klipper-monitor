7ae93415eace7b153ff9136822eb8381bca17c69
adam <adam@adammakesthings.dev>
2024-06-28 14:41:31 -0400

    Added some wifi stuff, still not working :(

--- main/main.c
+++ main/main.c
@@ -11,6 +11,7 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
@@ -32,6 +33,7 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) 
     {
+        ESP_LOGI(TAG,"Attempting to connect...");
         esp_wifi_connect();
     } 
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) 
@@ -48,7 +50,7 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
 
-        ESP_LOGD(TAG,"connect to the AP fail");
+        ESP_LOGI(TAG,"connect to the AP fail");
     } 
 
     else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) 
@@ -85,14 +87,55 @@ void wifi_init_sta(void)
                                                         NULL,
                                                         &instance_got_ip));
 
+    wifi_config_t wifi_config = { //constructor for wifi configuration
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) ); //set to station mode
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) ); //configure sta with previous config
+    ESP_ERROR_CHECK(esp_wifi_start() ); //start wifi with configuration
 
+    ESP_LOGI(TAG, "wifi_init_sta finished."); //:DDDD
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 WIFI_SSID, WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 WIFI_SSID, WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
 
 }
+
+void wifi_runner(void)
+{
+    esp_err_t ret = nvs_flash_init(); //initialise flash
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+}
+
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Hello world! :3");
     ESP_LOGI(TAG, "Set settings:\nSSID: %s \nPassword: %s",WIFI_SSID,WIFI_PASS);
+    wifi_runner();
     printf("\nExiting!");
 
 }
